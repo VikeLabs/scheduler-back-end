98397736054bb4f8655f079652d6ba8c64c2bcd1
Joel Kerfoot <joelkerfoot@gmail.com>
2019-11-06 19:48:51 -0800

    setup mongoose to retrieve and add courses

--- src/routes/courses.ts
+++ src/routes/courses.ts
@@ -1,3 +1,4 @@
+const Courses = require('../models/courses.model');
 import express = require('express');
 const router = express.Router();
 
@@ -6,31 +7,63 @@ interface Course {
   subject: string;
   code: string;
   term: string;
+  sections?: number[];
 }
 
-const COURSES: Array<Course> = require('../../temp/courses.json');
-
 router.get('/courses', (req, res) => {
-  // If a search and semester is provided seach for matches
+  let filter = {};
+
+  // If a search and semester is provided construct a filter
   if (req.query && req.query.search && req.query.term) {
     const { search, term } = req.query;
-
-    // Filter courses
-    const data = COURSES.filter((course: Course) => {
-      const { title, subject, code } = course;
-      const name = `${subject} ${code}`;
-
-      const match =
-        name.toLowerCase().includes(search.toLowerCase()) || title.toLowerCase().includes(search.toLowerCase());
-
-      return course.term === term && match;
-    });
-
-    res.json(data);
-    // Otherwise return all courses
-  } else {
-    res.json(COURSES);
+    // Create a filter that matches the semester and the search in any other key
+    filter = {
+      $and: [
+        { term: +term },
+        {
+          $or: [
+            { title: { $regex: search, $options: 'i' } },
+            { subject: { $regex: search, $options: 'i' } },
+            { code: +search || undefined },
+            { sections: +search || undefined },
+          ],
+        },
+      ],
+    };
   }
+
+  // Execute the query and return a response
+  Courses.find(filter)
+    .then((document: any) => {
+      res.json(document);
+    })
+    .catch((error: any) => res.status(500).json(error));
+});
+
+router.post('/courses', (req, res) => {
+  // Check if the course already exists
+  const query = {
+    subject: req.body.subject,
+    code: req.body.code,
+    term: req.body.term,
+  };
+  // The date to insert or update
+  const update = req.body;
+  // Use upsert option (insert if not exist)
+  const options = {
+    upsert: true,
+    new: true,
+    setDefaultsOnInsert: true,
+    useFindAndModify: false,
+  };
+
+  // Update or inster insert if not exsist and return response
+  Courses.findOneAndUpdate(query, update, options, (err: any, doc: any) => {
+    if (err) {
+      return res.status(500).json(err);
+    }
+    return res.json(doc);
+  });
 });
 
 module.exports = router;
