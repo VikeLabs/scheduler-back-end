8d0e80a2e07f23bf1b2bea2aaebb9d220e48c486
kvangorp <katelyn.vangorp@gmail.com>
2020-02-24 19:03:33 -0800

    Add a route to retrieve current active semesters

---
+++ src/routes/semesters.ts
@@ -0,0 +1,13 @@
+import express = require('express');
+const router = express.Router();
+
+router.get('/semesters', (req, res) => {
+  // Return hardcoded active semesters for now
+  const semesters = [
+    { term: 202001, title: 'Spring 2020' },
+    { term: 202005, title: 'Summer 2020' },
+  ];
+  return res.json(semesters);
+});
+
+module.exports = router;
