0b790fbaf9e9c60679ad01408859ab88ff07d3e2
Joel Kerfoot <joelkerfoot@gmail.com>
2020-03-10 21:12:11 -0700

    added the fall semester

--- src/routes/semesters.ts
+++ src/routes/semesters.ts
@@ -4,6 +4,7 @@ const router = express.Router();
 router.get('/semesters', (req, res) => {
   // Return hardcoded active semesters for now
   const semesters = [
+    { term: 201909, title: 'Fall 2019'},
     { term: 202001, title: 'Spring 2020' },
     { term: 202005, title: 'Summer 2020' },
   ];
